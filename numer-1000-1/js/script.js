c3022422a54a47ba2dd4429a874d4d51a40034cd
Alessandro Scolozzi <scolozzi.alessandro@gmail.com>
2022-06-07 12:27:17 +0200

    first commit

---
+++ numer-1000-1/js/script.js
@@ -0,0 +1,4 @@
+// Scriviamo in console tutti i numeri da 1000 a 1
+for(let i = 1000; i > 0; i--) {
+    console.log(i);
+}
---
+++ numeri-1-1000/js/script.js
@@ -0,0 +1,4 @@
+// Scriviamo in console tutti i numeri da 1 a 1000
+for(let i = 1; i <= 1000; i++) {
+    console.log(i);
+}
